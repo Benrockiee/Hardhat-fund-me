00f10e91978af379bc6a6ef4b950bfea728e63b5
Benrockiee <benrockiee@gmail.com>
2022-06-23 12:15:34 -0700

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,7 @@ const RINKEBY_RPC_URL =
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
-    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+    "01852cfcc86121cde28f9484acb3f47f399a564f4843e1ba289496ea5666846a"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
